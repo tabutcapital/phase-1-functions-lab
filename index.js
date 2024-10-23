7b6b5979454a56e33523812392ea980f70e88801
tabutcapital <iantabut3@gmail.com>
2024-10-23 17:59:24 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    switch(true) {
+        case distance === 43:
+            return 1;
+        case distance === 50:
+            return 8;
+        case distance === 34:
+            return 8;
+    }
+}
+function distanceFromHqInFeet(distance) {
+    switch(true) {
+        case distance === 43:
+            return 264;
+        case distance === 50:
+            return 2112;
+        case distance === 34:
+            return 2112;
+    }
+}
+function distanceTravelledInFeet(start, destination) {
+   switch (true) {
+    case (start < destination):
+        return (destination - start) * 264;
+    default:
+        return (start - destination) * 264;
+   }
+  }
+  
+    function calculatesFarePrice(start, destination) {
+        const distance = distanceTravelledInFeet(start, destination);
+        switch (true) {
+            case (distance <= 400):
+                return 0;
+            case (distance > 400 && distance <= 2000):
+                return (distance - 400) * 0.02;
+            case (distance > 2000 && distance <= 2500):
+                return 25;
+            default:
+                return 'cannot travel that far'
+            
+        }
+    } 
+
+        
+      
+        
+
+  
+
